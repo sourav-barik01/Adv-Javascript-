ce1133ce6b6f762159f19e88b12fea3b05e9aff3
Sourav Barik <119874718+sourav-barik01@users.noreply.github.com>
2024-01-17 20:00:43 +0530

    Create 11 - apply().js

---
+++ 4 - OOPs in JS/11 - apply().js
@@ -0,0 +1,3 @@
+// The apply() method of Function instances calls this function with a given this value, and arguments provided as an array (or an array-like object).
+
+// Will add the code if i find the best one
