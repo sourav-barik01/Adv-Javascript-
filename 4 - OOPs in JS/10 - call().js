91348b832ae764a0e8031986d6d2352fcbfe67e7
Sourav Barik <119874718+sourav-barik01@users.noreply.github.com>
2024-01-17 19:58:42 +0530

    Update 10 - call().js

--- 4 - OOPs in JS/10 - call().js
+++ 4 - OOPs in JS/10 - call().js
@@ -1,16 +1,18 @@
 // The call() method of Function instances calls this function with a given this value and arguments provided individually.
 
-function Product(name, price) {
-  this.name = name;
-  this.price = price;
-}
+const moduless = {
+  x: 42,
+  getX: function () {
+    return this.x;
+  },
+};
 
-function Food(name, price) {
-  Product.call(this, name, price);
-  this.category = 'food';
-}
+const unboundGetX = moduless.getX;
+console.log(unboundGetX());     // The function gets invoked at the global scope due to which this gives undefined
+// Expected output: undefined
 
-console.log(new Food('cheese', 5).name);
-// Expected output: "cheese"
+const value = unboundGetX.call(moduless);
+console.log(value);
+// Expected output: 42
 
 // Always keep in mind the major difference b/w bind() and call() is bind returns a new function whereas call() doesn't and works on same function
