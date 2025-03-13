fa30d029d8c56d210aa0aaebbfc0a99305d51866
Sourav Barik <119874718+sourav-barik01@users.noreply.github.com>
2025-03-13 21:30:28 +0530

    Create 0.6 - typeof & Historical Mistake Of Javascript.js

---
+++ 0. Javascript Basics Concepts/0.6 - typeof & Historical Mistake Of Javascript.js
@@ -0,0 +1,8 @@
+console.log(typeof "1"); // return string
+console.log(typeof 1); // return number
+console.log(typeof false); // return boolean
+console.log(typeof undefined); // return undefined
+
+// Historical Mistake of JS
+console.log(typeof null); /* return object but it should come as null bcz in ecma script doc of js datatypes null and object are different
+different datatypes but it is not changes / rectified bcz it could hamper many companies codebases */
