c0b9d840697692053b02ecfb4cc79cae088abeb4
Sourav Barik <119874718+sourav-barik01@users.noreply.github.com>
2025-03-13 14:58:43 +0530

    Update and rename 0.3 - Special Nos to 0.3 - Special Nos.js

--- 0. Javascript Basics Concepts/0.3 - Special Nos.js
+++ 0. Javascript Basics Concepts/0.3 - Special Nos.js
@@ -1,7 +1,7 @@
 // There are some special nos in javascript : 0, -ve 0, NaN, Infinity, -Infinity
 
-/* Negative 0 or -ve 0 - It indicates directional properties. Like a game subway suffer where velocity = 0 m/s but the player is facing forward or 
-backwards is indicated by -ve Zero.
+/* Negative 0 or -ve 0 - It indicates directional properties. Like a game subway suffer where velocity = 0 m/s but the player is facing 
+forward or backwards is indicated by -ve Zero.
 Neagtive Zero is considered as falsy value in javascript */
 
 // NaN - When you are bound to return a no, but there is no possible no to return then we return Nan(Not a Number)
