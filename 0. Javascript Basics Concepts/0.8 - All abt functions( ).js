a69e3f52f703160991fd12835b3319a31ef1c349
Sourav Barik <119874718+sourav-barik01@users.noreply.github.com>
2025-03-14 23:20:59 +0530

    Create 0.8 - All abt functions( ).js

---
+++ 0. Javascript Basics Concepts/0.8 - All abt functions( ).js
@@ -0,0 +1,7 @@
+function abc(x, y){   // This is known as function defination and inputs are known as parameter
+  return x+y;
+}
+
+let a = 10, b = 20;
+let ans = abc(10, 20); // This is calling a function and inputs are known as arguments
+console.log(ans);
