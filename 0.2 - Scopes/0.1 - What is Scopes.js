8f2f40c66ce31b9f95737b3a0b49017c2c9f127f
Sourav Barik <119874718+sourav-barik01@users.noreply.github.com>
2025-03-20 11:49:10 +0530

    Update 0.1 - What is Scopes.js

--- 0.2 - Scopes/0.1 - What is Scopes.js
+++ 0.2 - Scopes/0.1 - What is Scopes.js
@@ -1,1 +1,17 @@
+// What is Scopes ? -> It means visibility of variables & function
 
+/* Everything inside our code will be used in one of the following 2 ways
+a) Either it will be getting some values assigned to it
+b) Or some value will be retrieved from it */
+
+/* Do You think JS is compiled lang or interpreted lang ? -> Its a Hybrid of both Compiled & Interpreted
+--> Behaving like INTERPRETED
+Input :                                  Output :
+console.log("Hello, World!");            Hello, World!
+console.lo("Bye");                       Uncaught TypeError: console.lo is not a function
+--> Behaving like COMPILED
+Input :                                  Output :
+console.log("Hello World");              Uncaught SyntaxError: Unexpected Token '......'
+console......log("Bye"); */
+
+// We will study about how js execution takes place on next file...
