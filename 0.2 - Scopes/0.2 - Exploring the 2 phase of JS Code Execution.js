bc57be16bd233450f000cc4be024d0ddd0e30be0
Sourav Barik <119874718+sourav-barik01@users.noreply.github.com>
2025-03-20 14:26:56 +0530

    Update 0.2 - Exploring the 2 phase of JS Code Execution.js

--- 0.2 - Scopes/0.2 - Exploring the 2 phase of JS Code Execution.js
+++ 0.2 - Scopes/0.2 - Exploring the 2 phase of JS Code Execution.js
@@ -1,3 +1,4 @@
 JS Code Execution take place in 2 phases
-1) Parsing -> In parsing phase, scope resolution is decided
-2) Execution
+1) Parsing -> In parsing phase, scope resolution is decided due to which keys get a tag,
+              where they are which type of scope & syntax is checked..
+2) Execution -> In execution phase, values are assigned to key... & autoglobals takes place(Below to be read abt this)
