85c79ca0fccab324dfe7dfd29383c4e3961f0453
Sourav Barik <119874718+sourav-barik01@users.noreply.github.com>
2025-03-21 13:31:52 +0530

    Update 0.1 - What is HOF.js

--- 0.3 - Higher Order Function/0.1 - What is HOF.js
+++ 0.3 - Higher Order Function/0.1 - What is HOF.js
@@ -1,1 +1,11 @@
+function sayHello() {
+  return "Hello, ";
+}
+function greeting(helloMessage, name) {
+  console.log(helloMessage() + name);    // Output -> Hello, JavaScript!
+}
+greeting(sayHello, "JavaScript!");    // Pass `sayHello` as an argument to `greeting` function
+// HOF -> A function that returns a function or takes other functions as arguments is called a higher-order function. Eg: greeting()
+// The function that we pass as an argument to another function is called a callback function. sayHello() is a callback function.
 
+// Examples Of HOFs are : map, sort, filter, reduce etc...
