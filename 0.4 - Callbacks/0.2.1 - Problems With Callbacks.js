0dbff842274656902c7fdc9a19cfb04c73a72723
Sourav Barik <119874718+sourav-barik01@users.noreply.github.com>
2025-03-25 11:55:57 +0530

    Update and rename 0.2 - Problems With Callbacks.js to 0.2.1 - Problems With Callbacks.js

--- 0.4 - Callbacks/0.2.1 - Problems With Callbacks.js
+++ 0.4 - Callbacks/0.2.1 - Problems With Callbacks.js
@@ -1,5 +1,5 @@
 /*
---> Callback Hell (Pyramid of Doom)
+--> Callback Hell (Pyramid of Doom) : Basically it is a readibility problem, the code performs well and good
 1) When multiple asynchronous operations depend on each other, callbacks get deeply nested, making the code hard to read and maintain.
 2) Callback Hell in JS can be defined as the situation where we have nested callbacks(functions passed as arguments to other functions) 
 which makes the code difficult to read and debug. The term “callback hell” describes the deep nesting of functions that can result in 
@@ -25,7 +25,7 @@ function task5(callback){
     console.log("Hi i am task 5");
     callback();
 }
-task1(function(){
+task1(function(){        // This is callback hell
     task2(function(){
         task3(function(){
             task4(function(){
@@ -43,4 +43,4 @@ task1(function(){
 // Hi i am task 3
 // Hi i am task 4
 // Hi i am task 5
-Task Completed!!!
+// Task Completed!!!
