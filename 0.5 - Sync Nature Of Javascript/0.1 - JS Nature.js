d65e4dd9ff0c67f6770e847f601f85e6e9faa110
Sourav Barik <119874718+sourav-barik01@users.noreply.github.com>
2025-03-25 17:20:45 +0530

    Create 0.1 - JS Nature.js

---
+++ 0.5 - Sync Nature Of Javascript/0.1 - JS Nature.js
@@ -0,0 +1,8 @@
+/*
+--> JS is Sync in Nature
+--> JS is single-threaded
+In synchronous programming, operations are performed one after the other, in sequence. 
+So, basically each line of code waits for the previous one to finish before proceeding to the next. 
+This means that the program executes in a predictable, linear order, with each task being completed before the next one starts.
+*/
+// This only takes place if we excute only valid ECMAScript Code viz given by the standards...
