d68d1106f30a2a5e81c918c60b48820a2ff21591
Sourav Barik <119874718+sourav-barik01@users.noreply.github.com>
2025-03-31 23:44:16 +0530

    Create 0.3 Working Of Event Loop & Microtask Queue.js

---
+++ 1 - Promises/0.3 Working Of Event Loop & Microtask Queue.js
@@ -0,0 +1,28 @@
+console.log("Starting...");
+function createPromise() {
+  return new Promise(function exec(resolve, reject) {
+    console.log("Resolving the Promise")    // This is sync
+    resolve("Done");    
+  });
+}
+
+setTimeout(function process() {    // goes in callback queue or macrotask queue
+  console.log("Inside Timer");
+}, 1000);
+
+let p = createPromise();
+p.then(function fulfillHandler(value) {        // As we are resolving therefore fulfillHandler is going in microtask queue
+  console.log("We fulfilled with value", value);
+}, function rejectHandler(value) {
+  console.log("We Rejected with value", value);
+})
+
+for(let i=0; i<100000; i++) {}
+console.log("Ending...");
+
+// Output:
+// Starting...
+// Resolving the Promise
+// Ending...
+// We fulfilled with value Done
+// Inside Timer
