1279c7336ced0be8e0b599da7a03ad07b23b9ce4
Sourav Barik <119874718+sourav-barik01@users.noreply.github.com>
2025-04-03 15:20:05 +0530

    Create 0.4 Example Of Promises.js

---
+++ 1 - Promises/0.4 Example Of Promises.js
@@ -0,0 +1,29 @@
+// Eg : 1
+function fetchUrl(url) {
+  return new Promise(function(resolve, reject) {
+    console.log("Start Downloading from", url);
+    setTimeout(function processDownloading() {
+      let data = "Dummy Data";
+      console.log("Downloading Started...");
+      resolve(data);
+      console.log("Downloading Completed...");
+    }, 4000);
+  })
+}
+
+console.log("Start");
+let p = fetchUrl("www.google.com")
+p.then(function(value) {
+  console.log("Value is", value);
+});
+console.log("End");
+
+// Output:
+// Start
+// Start Downloading from www.google.com
+// End
+// Downloading Started...
+// Downloading Completed...
+// Value is Dummy Data
+
+/***********************************************************************************/ 
